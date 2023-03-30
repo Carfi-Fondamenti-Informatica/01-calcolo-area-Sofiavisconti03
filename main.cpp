1f97118c907a7b14a84334faa1f83dc6a0478879
Sofiavisconti03 <128122122+Sofiavisconti03@users.noreply.github.com>
2023-03-30 11:02:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-//int a;
-//float b;
-//char  c;
-
-cin >> a >> b >> c;
-
-//triangolo= (a*b)/2;
-//quadrato=a*a;
-//rettangolo=a*b;
-//trapezio=((a+b)*c)/2;
-
-
+    float a=0, b=0, c=0;
+    float triangolo=0, quadrato=0, rettangolo=0, trapezio=0;
+    cout<<"inserisci tre numeri reali"<<endl;
+    cin >> a >> b >> c;
+    triangolo= (a*b)/2;
+    quadrato= a*a;
+    rettangolo=a*b;
+    trapezio=((a+b)*c)/2;
+    cout<<triangolo<<endl;
+    cout<<quadrato<<endl;
+    cout<<rettangolo<<endl;
+    cout<<trapezio<<endl;
+    return 0;
 }
